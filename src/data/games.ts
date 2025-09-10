897abf069608399cbdda7b5d6d8d57c5f15df735
ilya <anihiro0505@gmail.com>
2025-09-10 15:22:09 +0500

    commit5

--- src/data/games.ts
+++ src/data/games.ts
@@ -11,19 +11,19 @@ export const games: Game[] = [
     {
         id: 'metro-2033',
         title: 'Metro 2033',
-        poster: "../../public/poster/MV5BMmQwYThjMjgtN2Q4Ny00MmZjLWEzYWUtNjUyNjE3NWViMWM0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        poster: "/poster/MV5BMmQwYThjMjgtN2Q4Ny00MmZjLWEzYWUtNjUyNjE3NWViMWM0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         description: 'Первая игра серии Metro, повествующая о путешествии Артёма к Полису.'
     },
     {
         id: 'metro-last-light',
         title: 'Metro: Last Light',
-        poster: '../../public/poster/metro_last_light_redux-720x1080.jpg',
+        poster: '/poster/metro_last_light_redux-720x1080.jpg',
         description: 'Продолжения истории Артёма и борьба за будущее метро.'
     },
     {
         id: 'metro-exodus',
         title: 'Metro Exodus',
-        poster: '../../public/poster/71khmtnH8fL._UF1000,1000_QL80_.jpg',
+        poster: '/poster/71khmtnH8fL._UF1000,1000_QL80_.jpg',
         description: 'Третья часть, в которой Артём покидает метро и отправляется на поверхность.'
     }
 ];
